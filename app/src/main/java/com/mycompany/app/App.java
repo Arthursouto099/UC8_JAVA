e62d359e0e77627f34d55007132a9e1bb69a229e
ARTHUR SANTOS TAVARES SOUTO <14255515999@senacrs.edu.br>
2025-03-24 13:29:49 -0300

    up

---
+++ app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app;
+
+/**
+ *
+ * @author ARTHURSANTOSTAVARESS
+ */
+public class App {
+
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ app/src/main/java/com/mycompany/app/contactModel/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.app.contactModel;
+
+/**
+ *
+ * @author ARTHURSANTOSTAVARESS
+ */
+public class User {
+    private String id;
+    private String name;
+    private String contact;
+    
+    public User(String id, String name, String contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    
+    
+}
